0d5f40df1d76a0ab805095b5f9c0a016405f2b49
Diego Duarte <projectdcd@gmail.com>
2020-09-20 20:17:42 -0500

    Only showing processes missing

--- javaScriptCode/Manager.js
+++ javaScriptCode/Manager.js
@@ -1,28 +1,86 @@
-import {Register} from '../javaScriptCode/Processes'
-
-const doc = document.getElementById("processes")
-
-
-register = new Register ()
-registers = []
-
-function isIdValid(id = 1){
-    if (registers == null){
-        return True
+// var self = this
+var registers = []
+function isIdValid (id) {
+    isValid = false
+    if (registers.length === 0){
+        isValid = true
+        return isValid
     }
     else{
-        for(x = 0; x < registers.length; x++){
-            if (x.id == id) {
-                return False
+        registers.forEach(elem => {
+            if(elem.id === id){
+                isValid = false
             }
             else{
-                return True
+                isValid = true
             }
-        }
+        })
+        return isValid
     }
 }
 
-let  result = document.getElementById("processes")
-let b = document.getElementById("getProcess")
-b.addEventListener("click")
-result.innerHTML = `Hi ${result}` 
+function getOperationsResult(){
+    let a = parseInt(document.getElementById("a").value)
+    let b = parseInt(document.getElementById("b").value)
+    let x = parseInt(document.getElementById("operation").value)
+    let result = 0
+    if (x === 1){
+        result = add(a, b)
+    }
+    else if (x === 2){
+        result = multiply(a, b)
+    }
+
+    else if (x === 3){
+        result = subtract(a, b)
+    }
+
+    else if (x === 4){
+        result = divide(a, b) 
+    }
+
+    else if (x === 5){
+        result = modulo(a, b)
+    }
+
+    else if (x === 6){
+        result = power(a, b)
+    }
+    return result
+}
+
+
+
+
+function totalForms () {
+    let processes = parseInt(document.getElementById("processes").value )
+    register = new Register ()
+    id = parseInt(document.getElementById("regId").value)
+    if (isIdValid(id) === true) {
+        register.id = id
+        register.name = document.getElementById("name").value
+        register.operation = getOperationsResult()
+        register.max_ex_time = document.getElementById("max_ex_time").value
+        registers.push(register)
+        document.getElementById("register").reset()
+        document.getElementById("processes").style.visibility = "hidden";
+    }
+    else {
+        document.getElementById("regId").value = "Enter a valid Id"
+    }
+    
+    if (registers.length === processes) {
+        document.getElementById("register").style.display="none";
+        showRegisters()
+    }
+    
+}
+
+function showRegisters (){
+    registers.forEach(elem => {
+        p = document.getElementById("RegistersForms")
+        p.innerHtml + elem.id
+        p.innerHtml += elem.name 
+    })
+}
+
